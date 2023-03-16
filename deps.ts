a748a775d46503db744fbec8e8641498e64b84f4
RJ <rongjihuang@gmail.com>
2023-03-16 14:59:03 +0800

    Upgrade to deno/std@0.180.0

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
 // Copyright 2023 the NextRJ organization. All Rights Reserved. MIT license.
 
 // deno standard library
-export { assertStrictEquals, assertThrows } from "https://deno.land/std@0.175.0/testing/asserts.ts"
-export { dirname, extname, join as joinPath } from "https://deno.land/std@0.175.0/path/mod.ts"
-export { writeAll } from "https://deno.land/std@0.175.0/streams/mod.ts"
-export { extension } from "https://deno.land/std@0.175.0/media_types/mod.ts"
+export { assertStrictEquals, assertThrows } from "https://deno.land/std@0.180.0/testing/asserts.ts"
+export { dirname, extname, join as joinPath } from "https://deno.land/std@0.180.0/path/mod.ts"
+export { writeAll } from "https://deno.land/std@0.180.0/streams/mod.ts"
+export { extension } from "https://deno.land/std@0.180.0/media_types/mod.ts"
 
 // 3-parts
 import stringWidth from "https://cdn.skypack.dev/string-width@5.1.2"
